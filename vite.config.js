eb55bf273930a863cc18fbdef2238cd23bc529f6
basiab11 <basiab1907@interia.pl>
2025-06-10 19:22:36 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { glob } from 'node:fs/promises';
 import { defineConfig } from 'vite';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
