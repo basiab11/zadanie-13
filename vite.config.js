c1070a305411fdf9c3782250df3fde93e58ecb62
basiab11 <basiab1907@interia.pl>
2025-06-10 18:41:47 +0200

    zmiana vite

--- vite.config.js
+++ vite.config.js
@@ -1,51 +1,37 @@
-import { dirname, resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { glob } from 'node:fs/promises';
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
+import { fileURLToPath } from 'node:url';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = resolve(fileURLToPath(import.meta.url), '..');
 
-async function getBuildInputs() {
-  const collectedInputs = [];
-  for await (const entry of glob('src/**/*.html')) {
-      console.log(resolve(__dirname, entry));
-      collectedInputs.push(resolve(__dirname, entry));
+export default defineConfig({
+  base: '/zadanie-13/',
+  root: resolve(__dirname, 'src'),
+  build: {
+    emptyOutDir: true,
+    outDir: resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        login: resolve(__dirname, 'src/login/index.html')
+      },
+      external: ['fsevents'],
+    },
+  },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
+  optimizeDeps: {
+    exclude: ['fsevents', 'lightningcss', 'chokidar'],
+  },
+  ssr: {
+    noExternal: ['fsevents', 'lightningcss', 'chokidar'],
+  },
+  resolve: {
+    dedupe: ['fsevents', 'lightningcss', 'chokidar']
   }
-  return collectedInputs;
-}
-
-export default defineConfig(async ({ command, mode }) => {
-  const resolvedInputs = await getBuildInputs();
-
-  return {
-    base: '/zadanie-13/',
-    plugins: [],
-    root: resolve(__dirname, 'src'),
-    build: {
-      emptyOutDir: true,
-      rollupOptions: {
-        input: resolvedInputs,
-      },
-      outDir: resolve(__dirname, 'dist'),
-    },
-    
-    server: {
-      watch: {
-        
-        usePolling: true,
-      },
-    },
-    
-    optimizeDeps: {
-      exclude: ['fsevents', 'lightningcss', 'chokidar'],
-    },
-    
-    ssr: {
-      noExternal: ['fsevents', 'lightningcss', 'chokidar'],
-    },
-  
-    resolve: {
-      dedupe: ['fsevents', 'lightningcss', 'chokidar']
-    }
-  };
 });
+
