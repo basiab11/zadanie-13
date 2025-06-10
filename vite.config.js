247cda34567a857edfa49a715c3d0fb2d99c3848
basiab11 <basiab1907@interia.pl>
2025-06-10 19:40:06 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,37 @@
 import { resolve } from 'node:path';
-    import { defineConfig } from 'vite'; 
-    import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+import { fileURLToPath } from 'node:url';
 
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = resolve(fileURLToPath(import.meta.url), '..');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = resolve(fileURLToPath(import.meta.url), '..');
 
-    export default defineConfig({
-      base: '/zadanie-13/',
-      root: resolve(__dirname, 'src'),
-      build: {
-        emptyOutDir: true,
-        outDir: resolve(__dirname, 'dist'),
-        rollupOptions: {
-          input: {
-            main: resolve(__dirname, 'src/index.html'),
-            login: resolve(__dirname, 'src/login/index.html')
-          },
-          external: ['fsevents'],
-        },
+export default defineConfig({
+  base: '/zadanie-13/',
+  root: resolve(__dirname, 'src'),
+  build: {
+    emptyOutDir: true,
+    outDir: resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        login: resolve(__dirname, 'src/login/index.html')
       },
-      server: {
-        watch: {
-          usePolling: true,
-        },
-      },
+      external: ['fsevents'],
+    },
+  },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
+  optimizeDeps: {
+    exclude: ['fsevents', 'lightningcss', 'chokidar'],
+  },
+  ssr: {
+    noExternal: ['fsevents', 'lightningcss', 'chokidar'],
+  },
+  resolve: {
+    dedupe: ['fsevents', 'lightningcss', 'chokidar']
+  }
+});
+
